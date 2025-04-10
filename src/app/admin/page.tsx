0c6c62a7a0f0f38e7ab300985277c6d014734e83
Arsene <komezarsene15@gmail.com>
2025-04-10 15:59:14 +0200

    fix redirect issues

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,34 +1,51 @@
-import { getAllPshyicalStoresByOwnerId } from '@/lib/actions/physical-store.action';
-import { getAllVirtualStoresByOwnerId } from '@/lib/actions/vitual-store.action';
-import { getAuthState } from '@/lib/user-label-permission';
-import { redirect } from 'next/navigation';
+'use client'
 
-export default async function AdminEntry() {
-    const {
-        isVirtualStoreOwner,
-        isPhysicalStoreOwner,
-        user,
-        isAuthenticated,
-        isSystemAdmin
-    } = await getAuthState();
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { getAllPshyicalStoresByOwnerId } from '@/lib/actions/physical-store.action'
+import { getAllVirtualStoresByOwnerId } from '@/lib/actions/vitual-store.action'
+import { getAuthState } from '@/lib/user-label-permission'
+import SpinningLoader from '@/components/spinning-loader'
 
-    if (!isAuthenticated || !user) redirect("/sign-in");
+export default function AdminEntry() {
+    const router = useRouter()
+    const [loading, setLoading] = useState(true)
 
-    if (isSystemAdmin) {
-        return (
-            <p>System Admin dashboard</p>
-        )
-    }
+    useEffect(() => {
+        const handleRedirect = async () => {
+            const {
+                isVirtualStoreOwner,
+                isPhysicalStoreOwner,
+                user,
+                isSystemAdmin
+            } = await getAuthState()
 
-    const stores = isVirtualStoreOwner
-        ? await getAllVirtualStoresByOwnerId(user.$id)
-        : isPhysicalStoreOwner
-            ? await getAllPshyicalStoresByOwnerId(user.$id)
-            : null
+            if (!user) {
+                router.replace('/sign-in')
+                return
+            }
 
-    if (!stores || stores.total === 0) {
-        redirect("/admin/stores/new")
-    } else {
-        redirect(`/admin/stores/${stores.documents[0].$id}`)
-    }
+            if (isSystemAdmin) {
+                setLoading(false)
+                return
+            }
+
+            const stores = isVirtualStoreOwner
+                ? await getAllVirtualStoresByOwnerId(user.$id)
+                : isPhysicalStoreOwner
+                    ? await getAllPshyicalStoresByOwnerId(user.$id)
+                    : null
+
+            if (!stores || stores.total === 0) {
+                router.replace('/admin/stores/new')
+            } else {
+                router.replace(`/admin/stores/${stores.documents[0].$id}`)
+            }
+        }
+
+        handleRedirect()
+    }, [router])
+
+    if (loading) return <SpinningLoader />
+    return <div>System Admin dashboard</div>
 }
